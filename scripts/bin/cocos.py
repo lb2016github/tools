5ae4f73e219ebd214db433e4c95b94610b3d6d9c
LuoBo <HelloLuoBo@126.com>
2019-09-09 14:37:05 +0800

    1）close default open config, as directly open ccs with cocosstudio have something wrong

--- scripts/bin/cocos.py
+++ scripts/bin/cocos.py
@@ -8,17 +8,76 @@ import argparse
 import os
 import bin_init
 from ccs_maker.ccs_maker import CCSMaker
+import shutil
 
 def publish_csb(res_dir, csd_name_list, ccs_file_name, cocos_dir, output_dir, open=False):
+	"""
+	发布csb
+	:param res_dir: cocos里面res和csb所在目录
+	:param csd_name_list: 需要发布的csd名字列表
+	:param ccs_file_name: 生成的ccs文件名
+	:param cocos_dir: cocosstudio的安装目录
+	:param output_dir: 生成文件的输出目录
+	:param open: 发布后，是否打开ccs工程
+	:return:
+	"""
 	res_dir = res_dir
 	target_path = os.path.join(res_dir, "../%s"%ccs_file_name)
 	ccs_maker = CCSMaker(res_dir, res_dir, csd_name_list, target_path)
 	ccs_maker.make()
+	# clear output_dir
+	print "Start clear output_dir: ", output_dir
+	if os.path.exists(output_dir):
+		shutil.rmtree(output_dir)
+	# publish ccs
+	print "Start publish ccs"
 	cocos_tool_path = os.path.join(cocos_dir, "Cocos.Tool.exe")
-	cmd = "%s publish -f %s -o %s -d Serializer_FlatBuffers"%(cocos_tool_path, target_path, output_dir)
+	cmd = "%s publish -f %s -o %s -d Serializer_FlatBuffers -w"%(cocos_tool_path, target_path, output_dir)
+	print "Start execute cmd", cmd
 	os.system(cmd)
+	# copy language res
+	print "Start copy language res"
+	copy_language_res(res_dir, output_dir)
 	if open:
 		open_ccs(cocos_dir, target_path)
+
+
+def copy_language_res(res_dir, out_dir):
+	"""
+	拷贝多语言资源
+	:param src_language_dir: 多语言资源
+	:param out_dir:
+	:return:
+	"""
+	print "=================Start copy language res==================="
+	src_language_dir = "%s/res/ui1/language"%res_dir
+	out_language_dir = "%s/res/ui1/language"%out_dir
+	print out_language_dir
+	# 检查是否有多语言资源输出
+	if not os.path.exists(out_language_dir):
+		return
+	filenames = []
+	for i in os.walk(out_language_dir):
+		filenames.extend(i[2])
+
+	def _get_rest_path(child_dir, parent_dir):
+		abs_child_path = os.path.abspath(child_dir)
+		abs_parent_path = os.path.abspath(parent_dir)
+		if abs_parent_path not in abs_child_path:
+			print "Error: %s is not child of %s" % (child_dir, parent_dir)
+			return
+		return abs_child_path[len(abs_parent_path) + 1:]
+	for parent_dir, child_dirs, child_files in os.walk(src_language_dir):
+		for file in child_files:
+			if file in filenames:
+				rest_path = _get_rest_path(parent_dir, src_language_dir)
+				dst_dir = os.path.join(src_language_dir, rest_path)
+				if not os.path.exists(dst_dir):
+					os.mkdir(dst_dir)
+				dst_path = os.path.join(dst_dir, file)
+				src_path = os.path.join(parent_dir, file)
+				print "copy: ", src_path, dst_path
+				shutil.copy(src_path, dst_path)
 
 def make_ccs(res_dir, csd_name_list, ccs_file_name, cocos_dir, open=False):
 	res_dir = res_dir
@@ -39,7 +98,6 @@ def init_parser():
 	parser.add_argument('--cocos_dir', help='CocosStudio install path', required=True)
 	parser.add_argument('--csd', nargs='+', help="csd list", default=[])
 	parser.add_argument("--ccs", default="default", help="ccs file name")
-	parser.add_argument("--open", default=False, type=bool, help="Open the project")
 
 	subparsers = parser.add_subparsers(dest="command", help="sub commands")
 
@@ -68,14 +126,14 @@ def run(args):
 	cocos_dir = args.cocos_dir
 	csd_name_list = args.csd
 	ccs_file_name = args.ccs
-	open = args.open
+	open = False
 
 	if args.command == "make_ccs":
 		make_ccs(res_dir, csd_name_list, ccs_file_name, cocos_dir, open)
 	elif args.command == "publish":
 		out_dir = args.out_dir
 		if not out_dir:
-			out_dir = os.path.join(res_dir, "../output_dir")
+			out_dir = os.path.join(res_dir, "../res")
 		publish_csb(res_dir, csd_name_list, ccs_file_name, cocos_dir, out_dir, open)
 
 
